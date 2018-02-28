114d2f114cf1b24297c402538b666af588ec6a20
Peter Monadjemi <pm@activetraining.de>
2018-02-28 12:14:59 +0100

    Kapitel 09 - Nachzüglerdateien hinzugefügt

--- Kapitel09 - DSC in der Praxis/09_Beispiel_WebAppEinrichten.ps1
+++ Kapitel09 - DSC in der Praxis/09_Beispiel_WebAppEinrichten.ps1
@@ -1,6 +1,9 @@
 <#
  .Synopsis
  Eine Web-App einrichten per DSC
+ .Notes
+ Kann nur unter Windows Server ausgeführt werden wegen WindowsFeature
+ Für Windows 10: Eventuell per Enable-WindowsOptionalFeature
 #>
 
 # Fügt das xWebAdministration-Modul per DSC hinzu
@@ -10,7 +13,7 @@ configuration InstallxWebAdministration
 
     Import-DSCResource -ModuleName PSDesiredStateConfiguration
 
-    Node $Computername
+    node $Computername
     {
         # xWebAdministration auf dem Node hinzufügen
         Script InstallxWebAdminResource
@@ -35,87 +38,88 @@ InstallxWebAdministration -Computername Localhost
 # Richtet die Website mit WebApp ein
 configuration PoshWebAppSetup
 {
-  Import-DSCResource -ModuleName PSDesiredStateConfiguration
-  Import-DSCResource -ModuleName xWebAdministration
+    Import-DSCResource -ModuleName PSDesiredStateConfiguration
+    Import-DSCResource -ModuleName xWebAdministration
 
-  node $AllNodes.NodeName
-  {
-    # Alle Webserver bezogenen Features hinzufügen
-    $i = 0
-    foreach($Feature in $Node.FeatureListe)
+    node $AllNodes.NodeName
     {
-        $i++
-        WindowsFeature "WebFeature$i"
+        # Alle Webserver bezogenen Features hinzufügen
+        $i = 0
+        foreach($Feature in $Node.FeatureListe)
+        {
+            $i++
+            WindowsFeature "WebFeature$i"
+            {
+                Ensure = "Present"
+                Name = $Feature
+            }
+        }
+
+        # App Pool einrichten
+        xWebAppPool PoshAppPool
         {
             Ensure = "Present"
-            Name = $Feature
+            Name = $Node.WebAppPoolName
+            State  = "Started"
+        }
+
+        # Website-Verzeichnis anlegen
+        File WebsiteDir
+        {
+            Ensure = "Present"
+            DestinationPath = $Node.WebSitePath
+            Type = "Directory"
+        }
+
+        xWebSite PoshWebSite
+        {
+            Ensure = "Present"
+            Name = $Node.WebsiteName
+            BindingInfo = MSFT_xWebBindingInformation
+            {
+                Protocol = "Http"
+                Port = $Node.Port
+            }
+            PhysicalPath = $Node.WebsitePath
+            State = "Started"
+            DependsOn = @("[xWebAppPool]PoshAppPool","[File]WebsiteDir")
+        }
+
+        xWebApplication PoshWebApp
+        {
+            Ensure = "Present"
+            Name = $Node.WebAppName
+            WebSite = $Node.WebsiteName
+            WebAppPool = $Node.WebAppPoolName
+            PhysicalPath = $Node.WebSitePath
+        }
+
+        File DefaultHtml
+        {
+            Ensure = "Present"
+            Type = "File"
+            DestinationPath = $Node.DefaultHtmlPath
+            SourcePath = $Node.DefaultHtmlSourcePath
+        }
+
+        File DefaultBitmap
+        {
+            Ensure = "Present"
+            Type = "File"
+            DestinationPath = $Node.DefaultImagePath
+            SourcePath = $Node.DefaultImageSourcePath
         }
     }
-
-    # App Pool einrichten
-    xWebAppPool PoshAppPool
-    {
-        Ensure = "Present"
-        Name = $Node.WebAppPoolName
-        State  = "Started"
-    }
-
-    # Website-Verzeichnis anlegen
-    File WebsiteDir
-    {
-      Ensure = "Present"
-      DestinationPath = $Node.WebSitePath
-      Type = "Directory"
-    }
-
-    xWebSite PoshWebSite
-    {
-      Ensure = "Present"
-      Name = $Node.WebsiteName
-      BindingInfo = MSFT_xWebBindingInformation
-      {
-          Protocol = "Http"
-          Port = $Node.Port
-        }
-        PhysicalPath = $Node.WebsitePath
-        State = "Started"
-        DependsOn = @("[xWebAppPool]PoshAppPool","[File]WebsiteDir")
-    }
-
-    xWebApplication PoshWebApp
-    {
-        Ensure = "Present"
-        Name = $Node.WebAppName
-        WebSite = $Node.WebsiteName
-        WebAppPool = $Node.WebAppPoolName
-        PhysicalPath = $Node.WebSitePath
-    }
-
-    File DefaultHtml
-    {
-        Ensure = "Present"
-        Type = "File"
-        DestinationPath = $Node.DefaultHtmlPath
-        SourcePath = $Node.DefaultHtmlSourcePath
-    }
-
-    File DefaultBitmap
-    {
-        Ensure = "Present"
-        Type = "File"
-        DestinationPath = $Node.DefaultImagePath
-        SourcePath = $Node.DefaultImageSourcePath
-    }
-  }
 }
 
 $ConfigData = @{
+
     AllNodes = @(
     @{
-      FeatureListe = @("Web-Server", "Web-Mgmt-Tools", "Web-Default-Doc",
-       "Web-Dir-Browsing", "Web-Http-Errors", "Web-Static-Content",
-       "Web-Http-Logging", "Web-Stat-Compression", "Web-Filtering",
-       "Web-CGI", "Web-ISAPI-Ext", "Web-ISAPI-Filter")
+        FeatureListe = @("Web-Server", "Web-Mgmt-Tools", "Web-Default-Doc",
+           "Web-Dir-Browsing", "Web-Http-Errors", "Web-Static-Content",
+           "Web-Http-Logging", "Web-Stat-Compression", "Web-Filtering",
+           "Web-CGI", "Web-ISAPI-Ext", "Web-ISAPI-Filter")
       NodeName="localhost"
       WebAppName = "PoshApp"
       WebAppPoolName = "PoshAppPool"
@@ -130,8 +134,8 @@ $ConfigData = @{
       DefaultImagePath = "C:\inetpub\wwwroot\poshsite\iis-85.png"
     }
     )
-  }
+}
   
-  PoshWebAppSetup -ConfigurationData $ConfigData
+PoshWebAppSetup -ConfigurationData $ConfigData
 
-  Start-DSCConfiguration -Path .\PoshWebAppSetup -Wait -Verbose -Force
+# Start-DSCConfiguration -Path .\PoshWebAppSetup -Wait -Verbose -Force
